9c6c7193f971c3e5bffc9718183df6c5281e5bcd
Yun Wu <yunwu@M08337-01.local>
2024-02-22 17:19:16 +0100

    Actual is not String

--- mn-quickstart/src/main/java/com/wy/udemy/HelloWorldTranslationConfig.java
+++ mn-quickstart/src/main/java/com/wy/udemy/HelloWorldTranslationConfig.java
@@ -3,6 +3,7 @@ package com.wy.udemy;
 import io.micronaut.context.annotation.ConfigurationProperties;
 import io.micronaut.serde.annotation.Serdeable;
 
+@Serdeable
 @ConfigurationProperties("hello.world.translation")
 public interface HelloWorldTranslationConfig {
     String getDe();
