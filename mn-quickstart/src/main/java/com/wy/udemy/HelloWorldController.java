9e49601dfdb66759139414c72d345952be0c45ea
Yun Wu <yunwu@M08337-01.local>
2024-02-22 15:03:55 +0100

    13.Logging

--- mn-quickstart/src/main/java/com/wy/udemy/HelloWorldController.java
+++ mn-quickstart/src/main/java/com/wy/udemy/HelloWorldController.java
@@ -4,6 +4,8 @@ import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import jakarta.inject.Inject;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 
 
 // First Application
@@ -19,6 +21,8 @@ import jakarta.inject.Inject;
 @Controller("/hello")
 public class HelloWorldController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(HelloWorldController.class);
+
     @Inject
     private final MyService service;
     public HelloWorldController(MyService service){
@@ -27,6 +31,7 @@ public class HelloWorldController {
 
     @Get(produces = MediaType.TEXT_PLAIN)
     public String helloWorld(){
+        LOG.debug("Called the hello World API.");
         return service.helloFromService();
     }
 }
