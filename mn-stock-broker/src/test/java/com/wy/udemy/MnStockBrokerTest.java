e046b6832d0088b87496523ce90f747507c624ed
Yun Wu <yunwu@M08337-01.local>
2024-02-27 00:06:07 +0100

    20:Http Routing-GET requests JUnit

--- mn-stock-broker/src/test/java/com/wy/udemy/MnStockBrokerTest.java
+++ mn-stock-broker/src/test/java/com/wy/udemy/MnStockBrokerTest.java
@@ -1,5 +1,10 @@
 package com.wy.udemy;
 
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.HttpStatus;
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.json.tree.JsonNode;
 import io.micronaut.runtime.EmbeddedApplication;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import org.junit.jupiter.api.Test;
@@ -7,15 +12,20 @@ import org.junit.jupiter.api.Assertions;
 
 import jakarta.inject.Inject;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @MicronautTest
-class MnStockBrokerTest {
+class SymbolsControllerTest {
 
     @Inject
-    EmbeddedApplication<?> application;
+    @Client("/symbols")
+    HttpClient client;
 
     @Test
-    void testItWorks() {
-        Assertions.assertTrue(application.isRunning());
+    void symbolsEndpointReturnsListofSymbol() {
+       var response = client.toBlocking().exchange("/", JsonNode.class);
+       assertEquals(HttpStatus.OK, response.getStatus());
+       assertEquals(10, response.getBody().get().size());
     }
 
 }
