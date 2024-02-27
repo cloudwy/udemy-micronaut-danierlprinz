640240df14924abeff732db736800eeb68a99624
Yun Wu <yunwu@M08337-01.local>
2024-02-27 12:04:05 +0100

    21: 1st test

--- mn-stock-broker/src/test/java/com/wy/udemy/SymbolsControllerTest.java
+++ mn-stock-broker/src/test/java/com/wy/udemy/SymbolsControllerTest.java
@@ -1,5 +1,7 @@
 package com.wy.udemy;
 
+import com.wy.udemy.broker.InMemoryStore;
+import com.wy.udemy.broker.Symbol;
 import io.micronaut.http.HttpStatus;
 import io.micronaut.http.client.HttpClient;
 import io.micronaut.http.client.annotation.Client;
@@ -7,6 +9,8 @@ import io.micronaut.json.tree.JsonNode;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
+
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -16,10 +20,23 @@ public class SymbolsControllerTest {
     @Client("/symbols")
     HttpClient client;
 
+    @Inject
+    InMemoryStore inMemoryStore;
+
     @Test
     void symbolsEndpointReturnsListofSymbol() {
         var response = client.toBlocking().exchange("/", JsonNode.class);
         assertEquals(HttpStatus.OK, response.getStatus());
         assertEquals(10, response.getBody().get().size());
+    }
+
+    @Test
+    void symbolsEndpointReturnsTheCorrectSymbol(){
+        var testSymbol = new Symbol("TEST");
+        inMemoryStore.getSymbols().put(testSymbol.value(), testSymbol);
+        Symbol symbols = inMemoryStore.getSymbols().get("TEST");
+        var response = client.toBlocking().exchange("/" + testSymbol.value(), JsonNode.class);
+        assertEquals(HttpStatus.OK, response.getStatus());
+        assertEquals(testSymbol.value(), response.getBody().get().get("value").getValue()); //attention!
     }
 }
