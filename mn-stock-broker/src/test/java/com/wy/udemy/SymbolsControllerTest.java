6986f9eeaea0cea9f82744214d1cc3b39e4a8f1f
Yun Wu <yunwu@M08337-01.local>
2024-02-27 10:27:58 +0100

    Rename SymbolsControllerTest

--- mn-stock-broker/src/test/java/com/wy/udemy/SymbolsControllerTest.java
+++ mn-stock-broker/src/test/java/com/wy/udemy/SymbolsControllerTest.java
@@ -1,31 +1,25 @@
 package com.wy.udemy;
 
-import io.micronaut.http.HttpResponse;
 import io.micronaut.http.HttpStatus;
 import io.micronaut.http.client.HttpClient;
 import io.micronaut.http.client.annotation.Client;
 import io.micronaut.json.tree.JsonNode;
-import io.micronaut.runtime.EmbeddedApplication;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions;
-
 import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @MicronautTest
-class SymbolsControllerTest {
-
+public class SymbolsControllerTest {
     @Inject
     @Client("/symbols")
     HttpClient client;
 
     @Test
     void symbolsEndpointReturnsListofSymbol() {
-       var response = client.toBlocking().exchange("/", JsonNode.class);
-       assertEquals(HttpStatus.OK, response.getStatus());
-       assertEquals(10, response.getBody().get().size());
+        var response = client.toBlocking().exchange("/", JsonNode.class);
+        assertEquals(HttpStatus.OK, response.getStatus());
+        assertEquals(10, response.getBody().get().size());
     }
-
 }
