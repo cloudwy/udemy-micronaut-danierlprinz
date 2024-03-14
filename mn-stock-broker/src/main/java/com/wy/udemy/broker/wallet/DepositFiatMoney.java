21f1b23115f12c5093ce499fdd6d9afb10a0424e
Yun Wu <yunwu@M08337-01.local>
2024-03-14 09:22:22 +0100

    fix up: Internal Server Error: No bean introspection available

--- mn-stock-broker/src/main/java/com/wy/udemy/broker/wallet/DepositFiatMoney.java
+++ mn-stock-broker/src/main/java/com/wy/udemy/broker/wallet/DepositFiatMoney.java
@@ -4,7 +4,9 @@ import com.wy.udemy.broker.Symbol;
 
 import java.math.BigDecimal;
 import java.util.UUID;
+import io.micronaut.serde.annotation.Serdeable;
 
+@Serdeable
 public record DepositFiatMoney(
         UUID acoountId,
         UUID walletId,
