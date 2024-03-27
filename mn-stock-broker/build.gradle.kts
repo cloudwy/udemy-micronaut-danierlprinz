7b476b2bea712469f242db9919ad04c82310bb54
Yun Wu <yunwu@M08337-01.local>
2024-03-27 11:36:03 +0100

    60. Scheme migration with FlyWay (comparison and sort order)

--- mn-stock-broker/build.gradle.kts
+++ mn-stock-broker/build.gradle.kts
@@ -15,49 +15,50 @@ repositories {
 }
 
 dependencies {
+    annotationProcessor("io.micronaut.data:micronaut-data-processor")
     annotationProcessor("io.micronaut:micronaut-http-validation")  //1
     annotationProcessor("io.micronaut.serde:micronaut-serde-processor") //n
-    implementation("io.micronaut.serde:micronaut-serde-jackson") //n
+    // https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-databind
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1") //2
+    implementation("io.micronaut:micronaut-management") //9
+    // https://mvnrepository.com/artifact/io.micronaut.flyway/micronaut-flyway
+    compileOnly("io.micronaut.flyway:micronaut-flyway") //3-implementation
+    implementation("io.micronaut.sql:micronaut-jdbc-hikari") // 10
+    // https://mvnrepository.com/artifact/jakarta.validation/jakarta.validation-api
+    implementation("jakarta.validation:jakarta.validation-api:3.1.0-M1") //4
     compileOnly("io.micronaut:micronaut-http-client") //5
+    testImplementation("io.micronaut:micronaut-http-client") //HttpClient in test
     runtimeOnly("ch.qos.logback:logback-classic") //6
-    testImplementation("io.micronaut:micronaut-http-client")
     // https://mvnrepository.com/artifact/com.github.javafaker/javafaker
     // https://github.com/DiUS/java-faker/issues/327
     implementation("com.github.javafaker:javafaker:1.0.2"){exclude(module = "snakeyaml")} //n
     implementation("org.yaml:snakeyaml:1.17") //n
-    // https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-databind
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1") //2
     // https://mvnrepository.com/artifact/org.projectlombok/lombok
     implementation("org.projectlombok:lombok") //n
     // https://mvnrepository.com/artifact/io.swagger.core.v3/swagger-annotations
     implementation("io.swagger.core.v3:swagger-annotations:2.2.20") //n
-    // https://mvnrepository.com/artifact/jakarta.validation/jakarta.validation-api
-    implementation("jakarta.validation:jakarta.validation-api:3.1.0-M1") //4
-    // https://mvnrepository.com/artifact/io.micronaut.security/micronaut-security
-//    implementation("io.micronaut.security:micronaut-security:4.6.9") //if exists, SymbolControllerTest failed
-    /*Schema Migration*/
-    // https://mvnrepository.com/artifact/io.micronaut.flyway/micronaut-flyway
-    compileOnly("io.micronaut.flyway:micronaut-flyway") //3-implementation
-    runtimeOnly("org.flywaydb:flyway-database-postgresql") //8
-    implementation("io.micronaut:micronaut-management")
+    implementation("io.micronaut.serde:micronaut-serde-jackson") //n
     /*Database Driver*/
     // https://mvnrepository.com/artifact/org.postgresql/postgresql
     implementation("org.postgresql:postgresql:42.7.3") //7
+    /*Schema Migration*/
+    runtimeOnly("org.flywaydb:flyway-database-postgresql") //8
 
 
-//    annotationProcessor("io.micronaut.data:micronaut-data-processor") //
+
+//    annotationProcessor("io.micronaut.data:micronaut-data-processor")
 //    annotationProcessor("io.micronaut:micronaut-http-validation") //1
 //    annotationProcessor("io.micronaut.validation:micronaut-validation-processor")
 //    implementation("io.micronaut:micronaut-jackson-databind") //2
-//    implementation("io.micronaut:micronaut-management")
-//    implementation("io.micronaut.data:micronaut-data-jdbc")
+////    implementation("io.micronaut:micronaut-management") //9
+////    implementation("io.micronaut.data:micronaut-data-jdbc")
 //    implementation("io.micronaut.flyway:micronaut-flyway") //3
-//    implementation("io.micronaut.sql:micronaut-jdbc-hikari")
-//    implementation("io.micronaut.validation:micronaut-validation")
+//    implementation("io.micronaut.sql:micronaut-jdbc-hikari") //10 important!
+////    implementation("io.micronaut.validation:micronaut-validation")
 //    implementation("jakarta.validation:jakarta.validation-api") //4
 //    compileOnly("io.micronaut:micronaut-http-client") //5
 //    runtimeOnly("ch.qos.logback:logback-classic") //6
-//    testImplementation("io.micronaut:micronaut-http-client")
+////    testImplementation("io.micronaut:micronaut-http-client")
 //    implementation("org.postgresql:postgresql:42.7.3") //7
 //    runtimeOnly("org.flywaydb:flyway-database-postgresql") //8
 //    runtimeOnly("org.yaml:snakeyaml")
